45bf760fba3c3923ac045b16b79c3245297500b2
Basile PRACCA <basile.pracca@gmail.com>
2024-06-03 18:08:57 +0200

    Split state and event management

---
+++ src/kiur/event.clj
@@ -0,0 +1,24 @@
+(ns kiur.event
+  (:require
+   [kiur.state :as state]))
+
+(defn- -event-type [_state event]  (:type event))
+(defmulti event #'-event-type)
+
+(defmethod event :mouse-clicked
+  [state _] (println state) state)
+(defmethod event :mouse-wheel
+  [state event]
+  state)
+(defmethod event :key-pressed
+  [state event]
+  (case (:key event)
+    :r state/default-state
+    state))
+(defmethod event :mouse-moved
+  [state event]
+  (update-in state [:controller :pointer]
+             assoc :x (:x event) :y (:y event)))
+(defmethod event :default
+  [state event]
+  state)
---
+++ src/kiur/state.clj
@@ -0,0 +1,5 @@
+(ns kiur.state)
+
+(def default-state {:player {:x 100 :y 100}
+                    :controller {:direction #{}}
+                    :debug false})
