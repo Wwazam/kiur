6e600e0c56268dd7f01d3c01f62624d79e320fad
Basile PRACCA <basile.pracca@gmail.com>
2024-06-03 22:34:18 +0200

    Bigger player

--- src/kiur/core.clj
+++ src/kiur/core.clj
@@ -12,7 +12,7 @@
                     {{px :x py :y} :pointer} :controller}]
   (when (and px py) (q/line x y px py))
   (q/with-translation [x y]
-    (q/ellipse 0 0 10 10)))
+    (q/ellipse 0 0 20 20)))
 (defn draw-state [state]
   (q/background 255)
   (draw-player state))
