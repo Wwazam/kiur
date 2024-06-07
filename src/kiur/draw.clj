1eba8887055a9606d749300a7108bbf0c454ba6b
Basile PRACCA <basile.pracca@gmail.com>
2024-06-07 12:58:54 +0200

    Remove cursor line

--- src/kiur/draw.clj
+++ src/kiur/draw.clj
@@ -4,7 +4,6 @@
 
 (defn draw-player [{{:keys [x y]} :player
                     {{px :x py :y} :pointer} :controller}]
-  (when (and px py) (q/line x y px py))
   (q/with-translation [x y]
     (q/ellipse 0 0 20 20)))
 (defn draw-state [state]
