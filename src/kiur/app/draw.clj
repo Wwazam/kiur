3cb0b5e5ceef796f7cba0e68c53972b91cded49d
Basile PRACCA <basile.pracca@gmail.com>
2024-06-10 00:28:18 +0200

    Clean draw function

--- src/kiur/app/draw.clj
+++ src/kiur/app/draw.clj
@@ -5,16 +5,17 @@
 (defn- draw-player [{{:keys [x y]} :player}]
   (q/with-translation [x y]
     (q/ellipse 0 0 20 20)))
+(defn draw-shape [building]
+  (let [{:keys [points color]} building]
+    (q/with-fill (apply q/color color)
+      (q/begin-shape)
+      (mapv (fn [p]
+              (apply q/vertex p))
+            points)
+      (q/end-shape))))
+
 (defn- draw-map [{:keys [map]}]
-  (mapv (fn [building]
-          (let [{:keys [points color]} building]
-            (q/with-fill (apply q/color color)
-              (q/begin-shape)
-              (mapv (fn [p]
-                      (apply q/vertex p))
-                    points)
-              (q/end-shape))))
-        map))
+  (mapv draw-shape map))
 (defn draw-state [state]
   (q/background 255)
   (draw-player state)
