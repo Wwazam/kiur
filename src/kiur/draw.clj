0e7e92e103121d7084f286033a12e916de7b27af
Basile PRACCA <basile.pracca@gmail.com>
2024-06-09 16:26:35 +0200

    Make map out of polygons

--- src/kiur/draw.clj
+++ src/kiur/draw.clj
@@ -1,6 +1,7 @@
 (ns kiur.draw
   (:require
-   [quil.core :as q]))
+   [quil.core :as q]
+   [kiur.polygon :as poly]))
 
 (defn- draw-player [{{:keys [x y]} :player
                      {{px :x py :y} :pointer} :controller}]
@@ -8,9 +9,13 @@
     (q/ellipse 0 0 20 20)))
 (defn- draw-map [{:keys [map]}]
   (mapv (fn [building]
-          (let [{:keys [x y w h color]} building]
+          (let [{:keys [points color]} building]
             (q/with-fill (apply q/color color)
-              (q/rect x y w h))))
+              (q/begin-shape)
+              (mapv (fn [p]
+                      (apply q/vertex p))
+                    points)
+              (q/end-shape))))
         map))
 (defn draw-state [state]
   (q/background 255)
--- src/kiur/state.clj
+++ src/kiur/state.clj
@@ -1,12 +1,14 @@
 (ns kiur.state
   (:require
-   [kiur.keymap :as keymap]))
+   [kiur.keymap :as keymap]
+   [kiur.polygon :as poly]))
 
 (defn default-state  []
   {:player {:x 100 :y 100 :speed 3}
    :controller {:direction #{}
                 :keymap keymap/dvorak-kb}
-   :map [{:type :rect :x 0 :y  0 :w  650 :h 50 :color [23 94 140]}
-         {:type :rect :x 50 :y  150 :w  12 :h 500 :color [94 140 23]}
-         {:type :rect :x 150 :y  30 :w  12 :h 500 :color [140 94 23]}]
+   :map [{:type :rect :points (poly/rect 0 0 650 50) :color [23 94 140]}
+         {:type :rect :points (poly/rect 50 150 12 500) :color [94 140 23]}
+         {:type :rect :points (poly/rect 150 30 12 500) :color [140 94 23]}
+         {:type :rect :points (poly/rect 90 5 65 50) :color [239 45 12]}]
    :debug false})
