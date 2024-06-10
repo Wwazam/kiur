8ea614290b3cd95ec8fc4a67375c396a60bba217
Basile PRACCA <basile.pracca@gmail.com>
2024-06-11 01:11:34 +0200

    Add fn and specs

--- src/kiur/geometry/vector.clj
+++ src/kiur/geometry/vector.clj
@@ -1,7 +1,8 @@
 (ns kiur.geometry.vector
   (:require
    [clojure.spec.alpha :as s]
-   [kiur.geometry.vector.spec :as vspec]))
+   [kiur.geometry.vector.spec :as vspec]
+   [kiur.geometry.pythagore :as ptgr]))
 
 (defn make-vector [point-1 point-2]
   (s/assert ::vspec/point point-1)
@@ -9,11 +10,17 @@
   (s/assert (fn points-have-same-dimension [points] (reduce = points)) (map count [point-1 point-2]))
   (mapv - point-2 point-1))
 
-(defn perpendicular [[x y]]
+(defn perpendicular [[x y :as vect]]
+  (s/assert ::vspec/vector vect)
   [y (- x)])
+
+(defn normalize [vec]
+  (let [h (apply ptgr/hypotenuse vec)]
+    (mapv #(/ % h) vec)))
 
 (defn dot-product [a b]
   (->> [a b]
+       (s/assert (s/coll-of (s/spec ::vspec/vector)))
        (apply map *)
        (reduce +)))
 
