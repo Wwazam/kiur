6e6884328317ccc5aedd55ebdece92c48d7fdf43
Basile PRACCA <basile.pracca@gmail.com>
2024-06-11 17:23:18 +0200

    Add polygon test

--- src/kiur/geometry/polygon.clj
+++ src/kiur/geometry/polygon.clj
@@ -6,7 +6,7 @@
    [(+ x w) (+ y h)]
    [x (+ y h)]])
 
-(defn- center [poly]
+(defn center [poly]
   (let [{:keys [x y count]} (reduce (fn [m [x y]] (-> m
                                                       (update :x + x)
                                                       (update :y + y)
@@ -16,15 +16,16 @@
     (mapv #(/ (double %) count) [x y])))
 (defn bounding-box [poly]
   (reduce (fn [m [x y]] (-> m
-                            (update-in [0 0] min x)
-                            (update-in [0 1] min y)
-                            (update-in [1 0] max x)
-                            (update-in [1 1] max y)))
-          [(first poly) (first poly)] poly))
+                            (update :min-x min x)
+                            (update :min-y min y)
+                            (update :max-x max x)
+                            (update :max-y max y)))
+          {:min-x ##Inf :max-x ##-Inf :min-y ##Inf :max-y ##-Inf}
+          poly))
 
-(defn- axis-aligned-inside? [[[x1 y1] [x2 y2]] [x y]]
-  (and (<= x1 x x2)
-       (<= y1 y y2)))
+(defn- axis-aligned-inside? [{:keys [min-x min-y max-x max-y]} [x y]]
+  (and (<= min-x x max-x)
+       (<= min-y y max-y)))
 
 (defn poly->edges [poly]
   (let [cycled-points (conj poly (first poly))]
--- test/kiur/geometry/polygon_test.clj
+++ test/kiur/geometry/polygon_test.clj
@@ -8,3 +8,15 @@
          (subject/rect 0 0 1 1)))
   (is (= [[0 5] [7 5] [7 18] [0 18]]
          (subject/rect 0 5 7 13))))
+
+(deftest center-test
+  (is (= [2.5 5.0]
+         (subject/center (subject/rect 0 0 5 10))))
+  (is (= [2.5 9.0]
+         (subject/center (subject/rect 0 4 5 10)))))
+
+(deftest bounding-box-test
+  (is (= {:min-x 0 :min-y 0 :max-x 5 :max-y 5}
+         (subject/bounding-box (subject/rect 0 0 5 5))))
+  (is (= {:min-x 0 :min-y 0 :max-x 9 :max-y 9}
+         (subject/bounding-box [[0 0] [1 5] [6 8] [3 2] [6 9] [9 0] [8 5]]))))
