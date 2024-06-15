b3f219042d7e2882390a224746f2dc23ac0b41e4
Basile PRACCA <basile.pracca@gmail.com>
2024-06-15 09:53:41 +0200

    Make player a ball

--- src/kiur/app/draw.clj
+++ src/kiur/app/draw.clj
@@ -3,8 +3,18 @@
    [quil.core :as q]))
 
 (defn- draw-player [{{:keys [x y r]} :player}]
-  (q/with-translation [x y]
-    (q/ellipse 0 0 r r)))
+  (q/with-fill [20 220 20]
+    (q/with-translation [x y]
+      (q/begin-shape)
+      (mapv #(->> %
+                  (mapv (partial * r))
+                  (apply q/vertex))
+            [[-1 0] [-2/3 -2/3]
+             [0 -1] [2/3 -2/3]
+             [1 0] [2/3 2/3]
+             [0 1] [-2/3 2/3]
+             [-1 0]])
+      (q/end-shape))))
 (defn draw-shape [building]
   (let [{:keys [points color]} building]
     (q/with-fill (apply q/color color)
