4a7edb90d586629b32c9b8826a57002144c0ac9a
Basile PRACCA <basile.pracca@gmail.com>
2024-08-01 21:13:19 +0200

    Dev

--- src/user.clj
+++ src/user.clj
@@ -1,11 +1,12 @@
 (ns user
   (:require
-   [clojure.spec.test.alpha :as stest]
-   [kiur.geometry.polygon :as p]
-   [kiur.geometry.polygon.spec :as poly-spec]
-   [clojure.spec.alpha :as s]))
+   [kiur.geometry.collision :as coll]
+   [kiur.app.state :as state]
+   [kiur.geometry.polygon :as p]))
 
-(time (stest/instrument `p/inside?))
+(defn valid? [m poly]
+  (some #(coll/collision? poly %) m))
 
-(s/assert ::poly-spec/point [2 1])
-(s/assert not (p/inside? [[0 1] [2 1] [4 2]] [2 1]))
+(let [state (state/default-state)
+      m (:map state)]
+  (valid? (p/octogon)))
