29abe9ac6ce1bc8fc1509a7db01d102909f21d77
Basile PRACCA <basile.pracca@gmail.com>
2024-08-09 16:23:58 +0200

    Remove dev stuff

--- src/user.clj
+++ src/user.clj
@@ -1,12 +1,1 @@
-(ns user
-  (:require
-   [kiur.geometry.collision :as coll]
-   [kiur.app.state :as state]
-   [kiur.geometry.polygon :as p]))
-
-(defn valid? [m poly]
-  (some #(coll/collision? poly %) m))
-
-(let [state (state/default-state)
-      m (:map state)]
-  (valid? (p/octogon)))
+(ns user)
